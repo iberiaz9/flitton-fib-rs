fa2a4e9a31c05d887d27de41384e973175a22112
iberiaz9 <alvarado.ja53@gmail.com>
2022-04-22 19:24:48 -0700

    Doing fibo in rust part

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools_rust import Binding, RustExtension
 
 
 setup(
-    name="ja-fib-rs",
+    name="flitton-fib-rs",
     version="0.1",
     rust_extensions=[RustExtension(
         ".flitton_fib_rs.flitton_fib_rs",
@@ -21,5 +21,10 @@ setup(
             "Operating System :: POSIX",
             "Operating System :: MacOS :: MacOS X",
         ],
+    entry_points={
+        'console_scripts': [
+            'fib-number = flitton_fib_rs.fib_number_command:fib_number_command',
+        ],
+    },
     zip_safe=False,
 )
