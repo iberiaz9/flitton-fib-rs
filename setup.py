07432a9ddb193bbf203eb431b8aa4341b282ff29
iberiaz9 <alvarado.ja53@gmail.com>
2022-04-23 18:05:32 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ from setuptools_rust import Binding, RustExtension
 
 
 setup(
-    name="flitton-fib-rs",
+    name="ja-fib-rs",
     version="0.1",
     rust_extensions=[RustExtension(
         ".flitton_fib_rs.flitton_fib_rs",
         path="Cargo.toml", binding=Binding.PyO3)],
-    packages=["flitton_fib_rs"],
+    packages=["ja_fib_rs"],
     classifiers=[
             "License :: OSI Approved :: MIT License",
             "Development Status :: 3 - Alpha",
